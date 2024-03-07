47a2a3082c29b1c1c400f2757546caf59f3dff27
Jan Roelof <>
2024-03-08 00:19:24 +0100

    added prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import MainNav from '@/components/MainNav'
+import { ThemeProvider } from '@/components/them-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
         <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
           <div className='max-w-6xl w-full'>
             <MainNav />
@@ -25,6 +27,7 @@ export default function RootLayout({
         </nav>
         
         <main className="flex flex-col items-center"><div className='max-w-6xl w-full'>{children}</div></main>
+        </ThemeProvider>
         </body>
     </html>
   )
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import ToggleMode from './toggleMode'
 
 const MainNav = () => {
   return (
@@ -11,7 +12,7 @@ const MainNav = () => {
         </div>
         <div className="flex items-center gap-2">
         <Link href="/logout">Logout</Link> 
-        <Link href="/dark">Dark</Link> 
+        <ToggleMode /> 
         </div>
 
     </div>
